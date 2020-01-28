94e918971a5ee0fcd340509d0e85470fafe09f97
Aman Aggarwal <aman@clickapps.co>
2020-01-28 14:42:38 +0530

    Initial Commit

---
+++ Alamofire-Tutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alamofire-Tutorial
+//
+//  Created by Aman Aggarwal on 28/01/20.
+//  Copyright © 2020 Aman Aggarwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
